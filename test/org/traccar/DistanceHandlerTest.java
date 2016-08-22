becdbf7aa2e241537f28126b2c755dbfbbc90fb0
INTERNO\jfarfan <jossgallagher@gmail.com>
2016-08-22 15:19:42 -0500

    no message

---
+++ test/org/traccar/DistanceHandlerTest.java
@@ -0,0 +1,21 @@
+package org.traccar;
+
+import org.junit.Test;
+import org.traccar.model.Position;
+
+import static org.junit.Assert.assertEquals;
+
+public class DistanceHandlerTest {
+
+    @Test
+    public void testCalculateDistance() throws Exception {
+
+        DistanceHandler distanceHandler = new DistanceHandler();
+
+        Position position = distanceHandler.calculateDistance(new Position());
+
+        assertEquals(0.0, position.getAttributes().get(Position.KEY_DISTANCE));
+
+    }
+
+}
